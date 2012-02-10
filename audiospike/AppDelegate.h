e248c656227567ffa9b7528ee502f752c42fa51c
Jay R. Wren <jrwren@xmtp.net>
2012-02-10 10:39:05 -0500

    Initial Commit

---
+++ audiospike/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  audiospike
+//
+//  Created by Jay R Wren on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ audiospike/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  audiospike
+//
+//  Created by Jay R Wren on 2/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
